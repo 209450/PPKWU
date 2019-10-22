df4329e68ab2a0243a67195ad58a044a8d53eec2
209450 <209450@edu.p.lodz.pl>
2019-10-22 16:52:59 +0200

    zad2

--- App.js
+++ App.js
@@ -5,12 +5,22 @@ const port = 3004
 
 const app = express()
 
+const specialChars = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g
+
 app.get("/:rev", (req, res) => {
     const stringRev = req.params.rev
-    let stringToSend = [...stringRev].reverse().join('') 
+    let stringToSend = [...stringRev].reverse().join('')
     res.send(stringToSend)
 })
 
+app.get("/checkletters/:str", (req, res) => {
+    const stringRev = req.params.str
+    const lower = /[a-z]/.test(stringRev)
+    const upper = /[A-Z]/.test(stringRev)
+    const special = specialChars.test(stringRev)
+    const result = { lowerCase: lower, uppercase: upper, special: special }
+    res.send(result)
+})
 
 app.listen(port, () => {
     console.log(`Endpoint na porcie:${port}`);
