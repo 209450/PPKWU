021dab7e9ccd1360896771d65ae1c24c4b06c072
209450 <209450@edu.p.lodz.pl>
2019-11-05 16:53:55 +0100

    pobieranie  eventów z weeia

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ const express = require("express")
 const fs = require("fs")
 const port = 3004
 
+var async = require('express-async-await')
+var fetch = require('node-fetch')
+var jsdom = require("jsdom")
+
 const app = express()
 
 const specialChars = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g
@@ -20,6 +24,23 @@ app.get("/checkletters/:str", (req, res) => {
     const special = specialChars.test(stringRev)
     const result = { lowerCase: lower, uppercase: upper, special: special }
     res.send(result)
+})
+
+app.get("/download/ics", async (req, res) => {
+    let htmlres
+    await fetch("http://www.weeia.p.lodz.pl")
+        .then((res) => res.text())
+        .then((html) => htmlres = html)
+        .catch((err) => console.log(err))
+
+    const dom = new jsdom.JSDOM(htmlres)
+    const document = dom.window.document
+
+    let active = document.querySelectorAll("td.active")
+    active.forEach(element => {
+        console.log(element.querySelector(":scope a.active").innerHTML)
+        console.log(element.querySelector(":scope p").innerHTML)
+    });
 })
 
 app.listen(port, () => {
