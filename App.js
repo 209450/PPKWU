598860ffe78602287d64da279968dc3155cde0a1
209450 <209450@edu.p.lodz.pl>
2019-11-19 16:14:22 +0100

    Endpoint - weeia ics

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ const express = require("express")
 const fs = require("fs")
 const port = 3004
 
+//HTML parse
 var async = require('express-async-await')
 var fetch = require('node-fetch')
 var jsdom = require("jsdom")
+
+//ICS generator
+const ics = require('ics')
 
 const app = express()
 
@@ -26,22 +30,66 @@ app.get("/checkletters/:str", (req, res) => {
     res.send(result)
 })
 
-app.get("/download/ics", async (req, res) => {
+app.get("/download/ics/:year/:month", async (req, res) => {
+    let { year, month } = req.params
+
+    if (isNaN(year) || isNaN(month) || !Number.isInteger(Number(year)) || !Number.isInteger(Number(month)) || year <= 0 || month <= 0 || month > 12) {
+        res.send("Invalid number!")
+    }
+
+    month < 10 ? month = '0' + month : month
+
+    const document = await htmlDOMfromURL(templateWeeiaURL({ year: year, month: month }))
+
+    events = []
+    let active = document.querySelectorAll("td.active")
+    active.forEach(element => {
+        day = element.querySelector(":scope a.active").innerHTML
+        title = element.querySelector(":scope p").innerHTML
+        events.push(ICSEvent(title, year, month, day))
+    });
+
+    try {
+        icsFileData = generateICSfromEvents(events)
+
+        fileName = `weeia_${year}_${month}.ics`
+        fs.writeFileSync(fileName, icsFileData)
+        res.download(fileName, (err) => {
+            fs.unlinkSync(fileName)
+        })
+    }
+    catch (e) {
+        res.send("ICS file generation error!")
+    }
+
+})
+
+
+
+async function htmlDOMfromURL(url) {
     let htmlres
-    await fetch("http://www.weeia.p.lodz.pl")
+    await fetch(url)
         .then((res) => res.text())
         .then((html) => htmlres = html)
         .catch((err) => console.log(err))
 
     const dom = new jsdom.JSDOM(htmlres)
-    const document = dom.window.document
+    return dom.window.document
+}
 
-    let active = document.querySelectorAll("td.active")
-    active.forEach(element => {
-        console.log(element.querySelector(":scope a.active").innerHTML)
-        console.log(element.querySelector(":scope p").innerHTML)
-    });
-})
+function templateWeeiaURL({ year, month }) {
+    return `http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=${year}&miesiac=${month}&lang=1`
+}
+
+function generateICSfromEvents(events) {
+    const { error, value } = ics.createEvents(events)
+    if (error || !value) throw error
+    else return value
+}
+
+function ICSEvent(title, year, month, day) {
+    return { title: title, start: [year, month, day], end: [year, month, day] }
+}
 
 app.listen(port, () => {
     console.log(`Endpoint na porcie:${port}`);
